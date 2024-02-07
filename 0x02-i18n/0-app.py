2d97cb912505ca4278dbe1ae18463afee284f9d8
MahmoudYoussefSdiek <mahmoudyoussefsdiekmd@gmail.com>
2024-02-07 00:50:04 +0000

    Create 0-app.py file

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module contains a simple Flask application.
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def get_index() -> str:
+    """
+    Return the index page.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
